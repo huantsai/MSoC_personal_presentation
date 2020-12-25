81a1b4f8c53d50688e092088a1a090c43e04e223
mhTsai <r08921080@ntu.edu.tw>
2020-12-25 18:48:24 +0800

    1st version

---
+++ xhls_Squared_difference_accumulate/src/directives.tcl
@@ -0,0 +1,9 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_interface -mode s_axilite "diff_sq_acc"
+set_directive_interface -mode s_axilite -depth 10 "diff_sq_acc" a
+set_directive_interface -mode s_axilite -depth 10 "diff_sq_acc" dout
+set_directive_interface -mode s_axilite -depth 10 "diff_sq_acc" b
